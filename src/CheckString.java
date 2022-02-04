daeb9da094b6327eba97915af37714704b1f26b8
saathvika <saathvika.g@zemosolabs.com>
2022-02-04 20:10:01 +0530

    initial commit

---
+++ src/CheckString.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class CheckString {
+
+    public static boolean containsAllLetters(String str){
+        StringBuilder sb = new StringBuilder();
+        sb.append("abcdefghijklmnopqrstuvwxyz");
+
+        String[] s = str.split("");
+
+        for(int i=0;i<str.length();i++){
+            if(sb.indexOf(s[i])!=-1){
+                int index=sb.indexOf(s[i]);
+                sb.replace(index,index+1,"");
+            }
+            else{
+                continue;
+            }
+        }
+
+        if(sb.toString().length()==0){
+            return true;
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = sc.nextLine();
+        input = input.toLowerCase().replace(" ","");
+        System.out.println(containsAllLetters(input));
+    }
+}
+
+//Time complexity : O(n)
+//Space Complexity :
